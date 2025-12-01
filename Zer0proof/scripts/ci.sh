10e8a648ac0e73d2705e3ad75872884a9166e024
Dwight-Modiwirijo <dwight@crypteum.io>
2025-12-01 22:57:36 +0100

    script ci

--- Zer0proof/scripts/ci.sh
+++ Zer0proof/scripts/ci.sh
@@ -12,10 +12,9 @@ lake build
 
 echo "[CI] Package"
 mkdir -p dist
-# kopieer alleen publieke .olean (géén tests)
+
 find .lake/build/lib/lean/AltRoute -maxdepth 1 -type f -name '*.olean' ! -name '*PublicTests*' -exec cp {} dist/ \;
 
-# metadata die je wilt meeleveren
 cp AltRoute/Interface.lean README.md LICENSE lean-toolchain lakefile.lean dist/
 
 ( cd dist && sha256sum * > SHA256SUMS && sha256sum -c SHA256SUMS >/dev/null )
